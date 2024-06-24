582dd7f72a4d72c92d993c5d98c6a11be952ee21
Fahim Morshed <bsse1102@iit.du.ac.bd>
2024-06-24 17:25:05 +0600

    task service

---
+++ PuppetMaster/src/main/java/com/fm/lifevoy/puppetmaster/PuppetMasterApplication.java
@@ -0,0 +1,13 @@
+package com.fm.lifevoy.puppetmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuppetMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PuppetMasterApplication.class, args);
+	}
+
+}
---
+++ PuppetMaster/src/test/java/com/fm/lifevoy/puppetmaster/PuppetMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fm.lifevoy.puppetmaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PuppetMasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
