02ca1cf9076e66f3c34f71d0141f916b63ff5d04
Fahim Morshed <bsse1102@iit.du.ac.bd>
2024-06-25 05:56:44 +0600

    reformate

--- GenerateFeed/src/main/java/com/fm/lifevoy/generatefeed/clients/PostClient.java
+++ GenerateFeed/src/main/java/com/fm/lifevoy/generatefeed/clients/PostClient.java
@@ -13,15 +13,15 @@ import static com.fm.lifevoy.generatefeed.clients.XMLUtils.*;
 public class PostClient {
     public static List<Post> getFeed(List<String> usernames) throws IOException {
         String xmlInput=
-                        "<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:gen=\"http://www.fm.com/lifevoy/post/generated\">\n" +
-                        "   <soapenv:Header/>\n" +
-                        "   <soapenv:Body>\n" +
-                        "      <gen:GetFeedRequest>\n";
-        for (String username : usernames) {
-            xmlInput += "           <gen:followingUsernames>"+username+"</gen:followingUsernames>\n";}
-        xmlInput+=      "      </gen:GetFeedRequest>\n" +
-                        "   </soapenv:Body>\n" +
-                        "</soapenv:Envelope>";
+                    "<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:gen=\"http://www.fm.com/lifevoy/post/generated\">\n" +
+                    "   <soapenv:Header/>\n" +
+                    "   <soapenv:Body>\n" +
+                    "      <gen:GetFeedRequest>\n";
+        for (String username : usernames) {  xmlInput +=
+                    "          <gen:followingUsernames>"+username+"</gen:followingUsernames>\n";}
+        xmlInput+=  "      </gen:GetFeedRequest>\n" +
+                    "   </soapenv:Body>\n" +
+                    "</soapenv:Envelope>";
 
         Document document = getParsedOutput(xmlInput,"http://localhost:8201/ws/");
 
@@ -30,31 +30,18 @@ public class PostClient {
 
         for(int i=0;i<nodeLst.getLength();i++){
             NodeList children = nodeLst.item(i).getChildNodes();
-            long postID = 0L;
-            for(int j=0;j<children.getLength();j++)
-                if(children.item(j).getNodeName().equals("ns2:postID")){
-                    postID= Long.parseLong(children.item(j).getTextContent());
-                    break;}
-            String text="";
-            for(int j=0;j<children.getLength();j++)
-                if(children.item(j).getNodeName().equals("ns2:text")){
-                    text=children.item(j).getTextContent();
-                    break;}
-            String posterUsername="";
-            for(int j=0;j<children.getLength();j++)
-                if(children.item(j).getNodeName().equals("ns2:posterUsername")){
-                    posterUsername=children.item(j).getTextContent();
-                    break;}
-            List<String> likers=new ArrayList<>();
-            for(int j=0;j<children.getLength();j++)
-                if(children.item(j).getNodeName().equals("ns2:likers"))
-                    likers.add(children.item(j).getTextContent());
 
             Post post = new Post();
-            post.setPostID(postID);
-            post.setText(text);
-            post.setPosterUsername(posterUsername);
-            post.getLikers().addAll(likers);
+            for(int j=0;j<children.getLength();j++){
+                String textContent = children.item(j).getTextContent();
+                String nodeName = children.item(j).getNodeName();
+                switch (nodeName) {
+                    case "ns2:postID" -> post.setPostID(Long.parseLong(textContent));
+                    case "ns2:text" -> post.setText(textContent);
+                    case "ns2:posterUsername" -> post.setPosterUsername(textContent);
+                    case "ns2:likers" -> post.getLikers().add(textContent);
+                }
+            }
             posts.add(post);
         }
         return posts;
